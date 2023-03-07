8c0c6e76c80da9c90b41af32d93e189ec9d38d90
Sebastian Schulz <Sebastian.Schulz@analytical-software.de>
2023-03-07 08:55:28 +0100

    just print all headers

--- app.py
+++ app.py
@@ -5,5 +5,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    fdid = request.headers.get('x-azure-fdid')
-    return f"Hello, World! This is my first azure webapp: {fdid}"
+    print(request.headers)
