b0f848113f46ef5d601efbe1cc731cbc553f23fa
Sebastian Schulz <Sebastian.Schulz@analytical-software.de>
2023-03-16 15:27:44 +0100

    create debug endpoints

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask, request
+from flask import Flask, request, json
+import requests
+import socket
 
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    fdid = request.headers.get('x-azure-fdid')
-    sfdid = request.headers.get('sx-azure-fdid')
-    return f"x-azure-fdid: {fdid}\nsx-azure-fdid: {sfdid}\n"
+def index():
+    hostname = socket.gethostname()
+
+    str = f"Request:\n  Hostname: {hostname}\n"
+    for h in request.headers.keys():
+        str += f"  {h}: {request.headers[h]}\n"
+
+    return str
+
+@app.route("/post", methods=['POST'])
+def repost():
+    data = json.loads(request.data)
+    x = requests.get(data["url"])
+
+    str = "Response:\n"
+    for h in x.headers.keys():
+        str += f"  {h}: {x.headers[h]}\n"
+
+    return f"{x.text}\n{str}"
